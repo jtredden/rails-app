c782fd7388467e2abf9d5252db1b91321b253d80
jtredden <software.imagineer@jtredden.net>
2012-10-20 23:37:58 -0600

    Initial rails app
    The result of 'rails new rails-app' to be expanded on for learning of
    the ruby language

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WebApp::Application.config.secret_token = '906a1779a349f8d10a67a704415186371ce01b8a3e0a6203fc6c6ccf3cdba5dd0fc01a0c643ccd93eb66b77c7d9ca07e9ebe4abf398f42daee51cfc399ae9655'
